e62bdfd8fa8738804fc9a9defad55c270ffa94df
Monique Chang <moniquechang1997@gmail.com>
2022-11-28 23:42:10 +0000

    working on solution for matrixElementsSum algo; trying to map through matrix and use for loop to look at each individual numbers

---
+++ matrixElementsSum/answer.js
@@ -0,0 +1,11 @@
+/* function solution(matrix) {
+  let total = 0;
+  matrix.map(index => {
+    for (let i = 0; i < index.length; i++) {
+      if (index[i] === 0) {
+        index[i] = 0
+      }
+    }
+  })
+
+} */
