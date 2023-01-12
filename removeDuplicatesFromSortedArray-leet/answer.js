47b495bf8575a53441b6dccc548ae8f9d743d40d
Monique Chang <moniquechang1997@gmail.com>
2023-01-12 12:52:53 +0000

    solution for remove duplicates from sorted array problem (leetcode)

---
+++ removeDuplicatesFromSortedArray-leet/answer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
+
+// added for commit
+removeDuplicates();
+
+// this answer is similar to remove element problem on leetcode1
