bb45199c500ada0667b1d2e7099a76c0a7fac1c7
Monique Chang <moniquechang1997@gmail.com>
2023-01-24 02:47:58 +0000

    solution for missing number problem (leetcode)

---
+++ missingNumber-leet/answer.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  nums.sort((a, b) => {
+    return a - b;
+  });
+  if (nums[0] !== 0) return 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] + 1 !== nums[i + 1]) {
+      return nums[i] + 1;
+    }
+  }
+  return nums.length;
+};
+
+// added for commit
+missingNumber();
