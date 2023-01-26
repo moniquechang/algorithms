06fad4e0d79d2077a58b14daf481fd7eeaaaa918
Monique Chang <moniquechang1997@gmail.com>
2023-01-26 03:50:30 +0000

    solution for find the difference problem (leetcode)

---
+++ findTheDifference-leet/answer.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function (s, t) {
+  /* const sArr = s.split('').sort();
+  const tArr = t.split('').sort();
+  while (sArr) {
+    if (sArr[0] !== tArr[0]) {
+      return tArr[0];
+    } else {
+      sArr.splice(0, 1);
+      tArr.splice(0, 1);
+    }
+  } */
+};
+
+// function above is commented out for commit
+
+// added for commit
+findTheDifference();
